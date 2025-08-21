f581864d08a863b693ac5bacfed1b18a0c83ddad
drypot <drypot@gmail.com>
2025-08-22 08:20:43 +0900

    add TextViewFactory

--- BrowseTextFiles/210 TextView/CustomTextEditor.swift
+++ BrowseTextFiles/210 TextView/CustomTextEditor.swift
@@ -13,34 +13,14 @@ struct CustomTextEditor: NSViewRepresentable {
     @Environment(AppSettings.self) var settings
 
     func makeNSView(context: Context) -> NSScrollView {
-        let textView = NSTextView(frame: .zero)
+        let textView = TextViewFactory.makeTextView()
+        textView.typingAttributes = makeAttr()
+        let scrollView = TextViewFactory.makeScrollView(textView)
 
-        textView.delegate = context.coordinator
+//        TextViewFactory.configureForNoWrap(textView, scrollView)
+//        textView.delegate = context.coordinator
 
-        textView.isEditable = true
-        textView.isRichText = false
-        textView.isSelectable = true
-
-        // maxSize 설정하고, isVerticallyResizable = true 해야 세로스크롤 할 수 있었다.
-        textView.maxSize = NSSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
-        textView.isVerticallyResizable = true
-        textView.autoresizingMask = [.width, .height]
-
-        textView.isHorizontallyResizable = false  // wrap 하려면 false
-        textView.textContainer?.widthTracksTextView = true  // wrap 하려면 true
-
-        let attrs = makeAttrs()
-
-        textView.typingAttributes = attrs
-        textView.backgroundColor = .textBackgroundColor
-
-        updateTextViewString(textView)
-
-        let scrollView = NSScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.hasVerticalScroller = true
-        scrollView.hasHorizontalScroller = true
-        scrollView.documentView = textView
+//        updateTextViewString(textView)
 
         return scrollView
     }
@@ -51,26 +31,41 @@ struct CustomTextEditor: NSViewRepresentable {
         }
     }
 
-    func makeAttrs() -> [NSAttributedString.Key: Any] {
+    func makeAttr() -> [NSAttributedString.Key : Any] {
         let defautFont = NSFont.systemFont(ofSize: settings.fontSize)
         let font = NSFont(name: settings.fontName, size: settings.fontSize) ?? defautFont
 
-        let paragraphStyle = NSMutableParagraphStyle()
-//        paragraphStyle.lineSpacing = settings.lineSpacing
-        paragraphStyle.lineHeightMultiple = 2.0
+        let lineHeightMultiple = 1.7
 
-        let attrs: [NSAttributedString.Key: Any] = [
+        let fontHeight = font.ascender + abs(font.descender) // + font.leading
+        let lineHeight = fontHeight * lineHeightMultiple
+        let baselineOffset = (lineHeight - fontHeight) / 2
+
+        // lineSpacing 을 쓰면 Enter 후 빈줄에서 커서가 커지는 현상이 발생한다.
+        // 문자를 입력하면 다시 줄어든다.
+
+        // lineHeightMultiple 을 쓰면 커서가 위로 삐죽이 올라온다.
+
+        // minimumLineHeight 를 주고 baselineOffset 을 조절하는 것이 무난해 보인다.
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 1.0
+        paragraphStyle.minimumLineHeight = lineHeight
+        paragraphStyle.maximumLineHeight = lineHeight
+
+        let attr: [NSAttributedString.Key : Any] = [
             .font: font,
-            .paragraphStyle: paragraphStyle
+            .paragraphStyle: paragraphStyle,
+            .baselineOffset: baselineOffset,
+            .foregroundColor: NSColor.textColor,
+            .backgroundColor: NSColor.textBackgroundColor,
         ]
 
-        return attrs
+        return attr
     }
 
     func updateTextViewString(_ textView: NSTextView) {
-        let attrs = makeAttrs()
-        let attrString = NSAttributedString(string: text, attributes: attrs)
-        textView.textStorage?.setAttributedString(attrString)
+        textView.string = text
     }
 
     func makeCoordinator() -> Coordinator {
@@ -78,14 +73,14 @@ struct CustomTextEditor: NSViewRepresentable {
     }
 
     class Coordinator: NSObject, NSTextViewDelegate {
-        var parent: CustomTextEditor
+        var view: CustomTextEditor
 
-        init(_ parent: CustomTextEditor) {
-            self.parent = parent
+        init(_ view: CustomTextEditor) {
+            self.view = view
         }
 
         func textViewDidChange(_ textView: NSTextView) {
-            self.parent.text = textView.string
+            self.view.text = textView.string
         }
     }
 }
