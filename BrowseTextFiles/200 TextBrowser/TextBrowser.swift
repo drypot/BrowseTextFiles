2965928ed1f0eabc4985f6e6507ee6612a25c2dd
drypot <drypot@gmail.com>
2026-03-28 23:41:19 +0900

    save/restore root url

--- BrowseTextFiles/200 TextBrowser/TextBrowser.swift
+++ BrowseTextFiles/200 TextBrowser/TextBrowser.swift
@@ -9,15 +9,17 @@ import SwiftUI
 import MyLibrary
 
 struct TextBrowser: View {
+    @Environment(\.openWindow) private var openWindow
     @Environment(SettingsData.self) var settings
 
+    @SceneStorage("rootURLData") private var rootURLData: Data?
+
     @State private var bufferManager = TextBufferManager()
-    @State private var showImporter = false
+
+    private var initRoot: URL?
 
     init(_ root: URL? = nil) {
-        if let root {
-            bufferManager.setRoot(to: root)
-        }
+        initRoot = root
     }
 
     var body: some View {
@@ -33,10 +35,7 @@ struct TextBrowser: View {
         } detail: {
             if bufferManager.root == nil {
                 Button("Open Folder") {
-                    showImporter = true
-                }
-                Button("Open Last Folder") {
-                    openLastOpenFolder()
+                    openFolder()
                 }
             } else {
                 VStack {
@@ -52,14 +51,9 @@ struct TextBrowser: View {
                                 .tag(buffer)
                         }
                     }
+                    //.tabViewCustomization(<#T##customization: Binding<TabViewCustomization>?##Binding<TabViewCustomization>?#>)
                 }
                 .padding()
-            }
-        }
-        .fileImporter(isPresented: $showImporter, allowedContentTypes: [.folder], allowsMultipleSelection: false) { result in
-            if case .success(let urls) = result {
-                guard let root = urls.first else { return }
-                openFolder(root)
             }
         }
         .onChange(of: bufferManager.selectedFolder) {
@@ -68,19 +62,73 @@ struct TextBrowser: View {
         .onChange(of: bufferManager.selectedFile) {
             bufferManager.openSelectedFile()
         }
+        .task {
+            if let root = initRoot {
+                saveRootURLAndOpenFolder(root)
+            } else {
+                if let root = loadRootURL() {
+                    saveRootURLAndOpenFolder(root)
+                }
+            }
+        }
+    }
+
+    func openFolder() {
+        let panel = NSOpenPanel()
+        panel.allowsMultipleSelection = false
+        panel.canChooseDirectories = true
+        panel.canChooseFiles = false
+        if panel.runModal() == .OK {
+            if let url = panel.url {
+                saveRootURLAndOpenFolder(url)
+            }
+        }
+    }
+
+    func saveRootURLAndOpenFolder(_ root: URL) {
+        saveRootURL(root)
+        openFolder(root)
     }
 
     func openFolder(_ root: URL) {
-        let bookmark = BookmarkManager.shared
-        bookmark.saveLastOpenFolder(root)
         bufferManager.setRoot(to: root)
     }
 
-    func openLastOpenFolder() {
-        let bookmark = BookmarkManager.shared
-        if let root = bookmark.loadLastOpenFolder() {
-            bufferManager.setRoot(to: root)
+//    func openLastOpenFolder() {
+//        let bookmark = BookmarkManager.shared
+//        if let root = bookmark.loadLastOpenFolder() {
+//            bufferManager.setRoot(to: root)
+//        }
+//    }
+
+    func saveRootURL(_ url: URL) {
+        do {
+            let securityScoped = url.startAccessingSecurityScopedResource()
+            defer { if securityScoped { url.stopAccessingSecurityScopedResource() } }
+            rootURLData = try url.bookmarkData(options: .withSecurityScope,
+                                               includingResourceValuesForKeys: nil,
+                                               relativeTo: nil)
+        } catch {
+            print("saving bookmark failed: \(error)")
         }
+    }
+
+    func loadRootURL() -> URL? {
+        do {
+            guard let data = rootURLData else { return nil }
+            var isStale = false
+            let url = try URL(resolvingBookmarkData: data,
+                              options: .withSecurityScope,
+                              relativeTo: nil,
+                              bookmarkDataIsStale: &isStale)
+            if isStale {
+                saveRootURL(url)
+            }
+            return url
+        } catch {
+            print("loading bookmark failed: \(error)")
+        }
+        return nil
     }
 }
 
