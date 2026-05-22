620e71aa5a876fd2169fe18e94f1a3f82df4aa02
drypot <drypot@gmail.com>
2026-05-22 14:00:42 +0900

    add URL.relativePath

---
+++ BrowseTextFiles Tests/URLUtilityTests.swift
@@ -0,0 +1,96 @@
+//
+//  URLUtilityTests.swift
+//  Browse Text Files Tests
+//
+//  Created by Kyuhyun Park on 5/22/26.
+//
+
+import Foundation
+import Testing
+
+struct URLUtilityTests {
+
+    @Test func testIsChild() throws {
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/alex/Documents/Test/file.txt")
+            #expect(child.isChild(of: parent) == false)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents")
+            #expect(child.isChild(of: parent) == false)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/")
+            #expect(child.isChild(of: parent) == false)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/file.txt")
+            #expect(child.isChild(of: parent) == true)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/Test/file.txt")
+            #expect(child.isChild(of: parent) == true)
+        }
+    }
+
+    @Test func testIsChildOrEqual() throws {
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/alex/Documents/Test/file.txt")
+            #expect(child.isChildOrEqual(to: parent) == false)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents")
+            #expect(child.isChildOrEqual(to: parent) == true)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/")
+            #expect(child.isChildOrEqual(to: parent) == true)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/file.txt")
+            #expect(child.isChildOrEqual(to: parent) == true)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/Test/file.txt")
+            #expect(child.isChildOrEqual(to: parent) == true)
+        }
+    }
+
+    @Test func testRelativePath() throws {
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/alex/Documents/Test/file.txt")
+            #expect(child.relativePath(from: parent) == nil)
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents")
+            #expect(child.relativePath(from: parent) == "")
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/")
+            #expect(child.relativePath(from: parent) == "")
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/file.txt")
+            #expect(child.relativePath(from: parent) == "file.txt")
+        }
+        do {
+            let parent = URL(fileURLWithPath: "/Users/drypot/Documents")
+            let child = URL(fileURLWithPath: "/Users/drypot/Documents/Test/file.txt")
+            #expect(child.relativePath(from: parent) == "Test/file.txt")
+        }
+    }
+}
---
+++ BrowseTextFiles/Utility/URLUtility.swift
@@ -0,0 +1,31 @@
+//
+//  URLUtility.swift
+//  BrowseTextFiles
+//
+//  Created by Kyuhyun Park on 5/22/26.
+//
+
+import Foundation
+
+extension URL {
+    func isChild(of parent: URL) -> Bool {
+        let childComponents = self.standardized.pathComponents
+        let parentComponents = parent.standardized.pathComponents
+        guard childComponents.count > parentComponents.count else { return false }
+        return childComponents.starts(with: parentComponents)
+    }
+
+    func isChildOrEqual(to parent: URL) -> Bool {
+        let childComponents = self.standardized.pathComponents
+        let parentComponents = parent.standardized.pathComponents
+        return childComponents.starts(with: parentComponents)
+    }
+
+    func relativePath(from parent: URL) -> String? {
+        let childComponents = self.standardized.pathComponents
+        let parentComponents = parent.standardized.pathComponents
+        guard childComponents.starts(with: parentComponents) else { return nil }
+        let relativeComponents = childComponents.dropFirst(parentComponents.count)
+        return relativeComponents.joined(separator: "/")
+    }
+}
