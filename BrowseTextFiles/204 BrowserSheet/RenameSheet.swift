ebec9620b0ae33d79c4b9aaca511c3d98e329049
drypot <drypot@gmail.com>
2026-07-06 17:32:43 +0900

    add selectNameWithoutExtension in RenameSheet

--- BrowseTextFiles/204 BrowserSheet/RenameSheet.swift
+++ BrowseTextFiles/204 BrowserSheet/RenameSheet.swift
@@ -8,70 +8,70 @@
 import SwiftUI
 
 struct RenameSheet: View {
-    @Environment(AppState.self) var appState
-    @Environment(BrowserState.self) var browserState
     @Environment(RenameState.self) var renameState
 
     @Environment(\.dismiss) private var dismiss
 
     @State private var orgName = ""
     @State private var newName = ""
+    @State private var selection: TextSelection?
+    @FocusState private var isFocused: Bool
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Form {
-                Section {
-                    HStack {
-                        Text("Rename")
-                            .frame(width: 60, alignment: .leading)
-                        Text(orgName)
+        Form {
+            Section {
+                LabeledContent("Rename") {
+                    Text(orgName)
+                }
+                TextField("to", text: $newName, selection: $selection)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.bottom)
+                    .focused($isFocused)
+                    .onChange(of: isFocused) { _, isFocused in
+                        if isFocused {
+                            selectNameWithoutExtension()
+                        }
                     }
-                    HStack {
-                        Text("to")
-                            .frame(width: 60, alignment: .leading)
-                        TextField("", text: $newName)
-                            .labelsHidden()
-                            .textFieldStyle(.roundedBorder)
-                            .frame(maxWidth: .infinity)
+            } footer: {
+                HStack {
+                    Button("Cancel") {
+                        dismiss()
                     }
+                    .keyboardShortcut(.escape)
+
+                    Button("OK") {
+                        submit()
+                        dismiss()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .keyboardShortcut(.defaultAction)
                 }
             }
-            .formStyle(.grouped)
-
-            HStack {
-                Spacer()
-
-                Button("Cancel") {
-                    dismiss()
-                }
-                .keyboardShortcut(.escape)
-
-                Button("OK") {
-                    submit()
-                    dismiss()
-                }
-                .buttonStyle(.borderedProminent)
-                .keyboardShortcut(.defaultAction)
-            }
-            .padding()
         }
+        .formStyle(.columns)
+        .padding()
         .frame(width: 500)
-        .onAppear {
-            loadSheet()
+        .task {
+            initSheet()
         }
     }
 
-    func loadSheet() {
+    func initSheet() {
         guard let name = renameState.renamingURL?.lastPathComponent else { return }
         orgName = name
         newName = name
     }
 
+    func selectNameWithoutExtension() {
+        if let dotIndex = newName.lastIndex(of: ".") {
+            let range = newName.startIndex..<dotIndex
+            selection = TextSelection(range: range)
+        } else {
+            selection = TextSelection(range: newName.startIndex..<newName.endIndex)
+        }
+    }
+
     func submit() {
         renameState.rename(with: newName)
     }
-}
-
-#Preview {
-    // RenameFileSheet()
 }
