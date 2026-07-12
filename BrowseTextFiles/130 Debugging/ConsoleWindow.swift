84dd21a3a13b99eafe2afecb2b70a3cc70ec2ee1
drypot <drypot@gmail.com>
2026-07-13 06:11:13 +0900

    disable console window

--- BrowseTextFiles/130 Debugging/ConsoleWindow.swift
+++ BrowseTextFiles/130 Debugging/ConsoleWindow.swift
@@ -11,17 +11,17 @@ struct ConsoleWindow: Scene {
     @Environment(\.openWindow) private var openWindow
 
     var body: some Scene {
-        Window("Debugging Console", id: "console") {
-            LogStoreView()
-        }
-        .commands {
-            CommandGroup(after: .help) {
-                Button("Debugging Console") {
-                    openWindow(id: "console")
-                }
-                .keyboardShortcut("c", modifiers: [.command, .option])
-            }
-        }
+        // Window("Debugging Console", id: "console") {
+        //     LogStoreView()
+        // }
+        // .commands {
+        //     CommandGroup(after: .help) {
+        //         Button("Debugging Console") {
+        //             openWindow(id: "console")
+        //         }
+        //         .keyboardShortcut("c", modifiers: [.command, .option])
+        //     }
+        // }
     }
 }
 
